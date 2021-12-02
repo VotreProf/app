8a863058a6239cf274dcd9e47f767744a38965c6
VotreProf <94190238+VotreProf@users.noreply.github.com>
2021-12-02 06:47:54 +0100

    Add files via upload

--- script/boutton.js
+++ script/boutton.js
@@ -2,3 +2,7 @@ function salle(){
     document.getElementById('signup').style.display = 'block';
     document.getElementById('home-home').style.display = 'none';
 }
+function fermer(){
+    document.getElementById('signup').style.display ='none';
+    document.getElementById('home-home').style.display = 'block';
+}
