014ce81058924173151ab8c4c2df7a587597a176
VotreProf <94190238+VotreProf@users.noreply.github.com>
2021-12-30 00:13:48 +0100

    Delete script directory

--- script/boutton.js
+++
@@ -1,8 +0,0 @@
-function salle(){
-    document.getElementById('signup').style.display = 'block';
-    document.getElementById('home-home').style.display = 'none';
-}
-function fermer(){
-    document.getElementById('signup').style.display ='none';
-    document.getElementById('home-home').style.display = 'block';
-}
